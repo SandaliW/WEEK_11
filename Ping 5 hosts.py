376c173e4c73b57297a332a80f342dcd2710db12
SandaliW <49431773+SandaliW@users.noreply.github.com>
2019-05-21 12:59:28 +0300

    Update Ping 5 hosts.py

--- Ping 5 hosts.py
+++ Ping 5 hosts.py
@@ -1,1 +1,13 @@
-print ('hi')
+import os
+import fnmatch
+from os import path
+from os import listdir
+
+onlyfiles = [f for f in listdir('.')]
+print (onlyfiles)
+
+b = os.walk('onlyfiles')
+f = []
+for(dirpath, dirnames, filenames) in b:
+    f.extend(os.path.splitext(google_)[0] for name in filenames)
+    break
